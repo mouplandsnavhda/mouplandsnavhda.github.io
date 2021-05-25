74188d9ffc364a4b6b9917b516390f1655c6aff5
B Whitfield <bwhitfield@github.com>
2021-05-25 07:05:14 -0500

    simplify - bw

--- analysis/scripts/index.js
+++ analysis/scripts/index.js
@@ -2,13 +2,17 @@ var gs2010 = document.URL.includes('localhost') ? `${document.URL}data/gs1.json`
 var gs2015 = document.URL.includes('localhost') ? `${document.URL}data/gs2.json` : 'https://www.mouplands.org/test/data/gs2.json';
 var gw2010 = document.URL.includes('localhost') ? `${document.URL}data/gw1.json` : 'https://www.mouplands.org/test/data/gw1.json';
 var gw2015 = document.URL.includes('localhost') ? `${document.URL}data/gw2.json` : 'https://www.mouplands.org/test/data/gw2.json';
-var gdata;
-        var tickss = [];
-        var ct = 1;
-        while(ct <= 204){
-            tickss.push(ct);
-            ct++;
-        }
+var gdata = [];
+function genTicks(){
+    var tickss = [];
+    var ct = 1;
+    while(ct <= 204){
+        tickss.push(ct);
+        ct++;
+    }
+    return tickss;
+}
+       
 var options = {
     legend: { position: 'none' },
     hAxis: {
@@ -16,7 +20,7 @@ var options = {
             min: 0,
             max: 204
         },
-        ticks: tickss
+        ticks: genTicks()
     },
     vAxis: { 
         viewWindowMode:'explicit',
@@ -38,54 +42,43 @@ function begin() {
         cache: false,
         dataType: "json",
         success: calc10
-    })
-        .done(function () {
-            console.log("done");
-        });
+    });
     $.ajax({
         url: gs2015,
         method: 'GET',
         cache: false,
         dataType: "json",
         success: calc15
-    })
-        .done(function () {
-            console.log("done");
-        });
+    });
 }
+
 
 function calc10(data) {
-    gdata = data.data;
-    var slimData = gdata.map((x) => {
-        return [x[1], x[20]]
-    });
-    populateChart10(slimData)
+    gdata.push(...data.data);
+    var slimData = slimDatas(data);
+    populateChart(slimData, 'gs_2010', 'GS Utility scores from 10 - 15')
 }
 function calc15(data) {
-    gdata = data.data;
-    var slimData = gdata.map((x) => {
-        return [x[1], x[20]]
-    });
-    populateChart15(slimData)
+    gdata.push(...data.data);
+    var slimData = slimDatas(data);
+    populateChart(slimData, 'gs_2015', 'GS Utility scores from 16 - 21')
 }
 
-function populateChart10(datas) {
+function slimDatas(data){
+    var d = data.data.map((x) => {
+        return [x[1], x[20]]
+    });
+    return d;
+}
+
+function populateChart(datas, divId, title) {
     google.charts.load("current", { packages: ["corechart"] });
     google.charts.setOnLoadCallback(drawChart);
         function drawChart() {
             var data = google.visualization.arrayToDataTable(datas);
-            var chart = new google.visualization.Histogram(document.getElementById('gs_2010'));
-            options.title = `GS utility test scores from 2010 - 2015. dogs tested: ${datas.length}`
+            var chart = new google.visualization.Histogram(document.getElementById(divId));
+            options.title = `${title}. dogs tested: ${datas.length}`
             chart.draw(data, options);
         }
 }
-function populateChart15(datas) {
-    google.charts.load("current", { packages: ["corechart"] });
-    google.charts.setOnLoadCallback(drawChart);
-        function drawChart() {
-            var data = google.visualization.arrayToDataTable(datas);
-            var chart = new google.visualization.Histogram(document.getElementById('gs_2015'));
-            options.title = `GS utility test scores from 2016 - 2021. dogs tested: ${datas.length}`
-            chart.draw(data, options);
-        }
-}
+
